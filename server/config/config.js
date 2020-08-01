95d739665ca8773f7fd5e395a0eb6d5525110bf1
Yiris <yiris@outlook.cl>
2020-08-01 03:47:40 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if( process.env.NODE_ENV === 'dev' ) {
     urlDB = 'mongodb://192.168.0.18:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://nelsick:Fzl8QgmckITVvKSg@cluster0.j67e3.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
